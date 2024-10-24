053e2e11c6daba2cf12e63bf3ad28ebc7382d7b8
booyaalex <64170386+booyaalex@users.noreply.github.com>
2024-10-24 16:52:58 -0400

    sf

--- js/canvas.js
+++ js/canvas.js
@@ -1,13 +1,25 @@
+let current_slide = 0;
+let slide_data_array = [];
+
 window.onload = function () {
+    //Define Canvas
     board = document.getElementById("myCanvas");
     ctx = board.getContext("2d");
 
+    //Update Slideshow
     requestAnimationFrame(update);
 };
 
 function update() {
     requestAnimationFrame(update);
-    ctx.clearRect(0, 0, boardWidth, boardHeight);
+    ctx.clearRect(0, 0, screen.availWidth, screen.availHeight);
 
 
 }
+
+async function getSlideshowData() {
+    const response = await fetch("json/slideData.json");
+    const JSON = await response.json();
+    console.log(JSON);
+}
+getSlideshowData();
